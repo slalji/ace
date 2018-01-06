fd3cb3cd50aeeab914ae335165368b518935db46
slalji <slalji@yahoo.com>
2018-01-06 11:57:35 +0300

    Initial commit

---
+++ ajax/getItem.php
@@ -0,0 +1,43 @@
+<?php
+require_once '../includes/db.php'; // The mysql database connection script
+$filter = '';
+$where = '';
+$flag = false;
+
+if(isset($_GET['id'])){
+	$where ='where ';
+	$filter = "groupid like '%".$mysqli->real_escape_string($_GET['id'])."%'";
+}
+if(isset($_GET['phone'])){
+	$where ='where ';
+	$filter .= " and msisdn like '%".$mysqli->real_escape_string($_GET['phone'])."%'";
+}
+if(isset($_GET['ref'])){
+	$where ='where ';
+	$filter .= " and reference like '%".$mysqli->real_escape_string($_GET['ref'])."%'";
+}
+
+if(isset($_GET['startDate']) && $_GET['startDate'] != ''){
+	$filter .= " and fulltimestamp >= '".$mysqli->real_escape_string($_GET['startDate'])."'";
+}
+if(isset($_GET['endDate'])&& $_GET['endDate'] != ''){
+	$filter .= " and fulltimestamp <= '".$mysqli->real_escape_string($_GET['endDate'])."'";
+}
+if (!isset($_GET['startDate']) && !isset($_GET['endDate'])  && !isset($_GET['id'] ))
+	$query="SELECT id, fulltimestamp,msisdn, account,service,reference, amount,tstatus,lang, groupid from transactions order by fulltimestamp desc";
+else
+	$query="SELECT id, fulltimestamp,msisdn, account,service,reference, amount,tstatus,lang, groupid from transactions $where $filter  order by fulltimestamp desc";
+
+$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+$arr = array();
+if($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		$arr[] = $row;
+	}
+}
+
+//echo $query ."<p>";
+# JSON-encode the response
+echo $json_response = json_encode($arr);//.$query;
+?>
---
+++ assets/js/project.js
@@ -0,0 +1,128 @@
+$( document ).ready(function() {
+
+
+    var lang = {};
+    lang['SW'] = 'Swahili';
+
+    var status ={};
+    status['SUCCESS'] = '<span class="label label-sm label-success">Success</span>';
+    status['FAILED'] = '<span class="label label-sm label-danger">Failed</span>';
+
+
+
+    //groupid
+    console.log( "ready!" );
+    console.log();
+    $.ajax({
+        type: 'get',
+        url: 'ajax/getGroup.php',
+        dataType: 'json',
+
+        success: function (response) {
+
+            var str = "<option value=''>ALL Groups</option>";
+            $.each(response, function(index, element) {
+
+                str += "<option value='"+element.groupid+"'>"+element.name+"</option>";
+
+            });
+
+            document.getElementById("groupname").innerHTML=str+"</select>";
+            $('#groupname').html(str);
+
+
+        }
+    });
+    // main content
+    $.ajax({
+        type: 'get',
+        url: 'ajax/getItem.php',
+        dataType: 'json',
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(JSON.stringify(jqXHR));
+            console.log("AJAX error: " + textStatus + ' : ' + errorThrown);
+        },
+        success: function (response) {
+            var str='';
+            $.each(response, function(index, element) {
+
+                str += "<tr><td>"+element.id+"</td>"+
+                "<td>"+element.fulltimestamp+
+                "<td>"+element.msisdn+"</td>"+
+                "<td>"+element.account+"</td>"+
+                "<td>"+element.service+"</td>"+
+                "<td>"+element.reference+"</td>"+
+                "<td>"+element.amount+"</td>"+
+                "<td>"+status[element.tstatus]+"</td>"+
+                "<td>"+lang[element.lang]+"</td>"+
+                "<td>"+element.groupid+"</td>"+
+                "</tr>";
+
+                //console.log(JSON.stringify(str));
+            });
+            document.getElementById("transaction_table").innerHTML=str;
+        }
+    });
+    $('#daterange').daterangepicker();
+    $('#daterange').on('apply.daterangepicker', function(ev, picker) {
+        console.log(picker.startDate.format('YYYY-MM-DD'));
+        console.log(picker.endDate.format('YYYY-MM-DD'));
+    });
+    $('#calshow').change(function() {
+       var ch =  $('#calshow').val(this.checked);
+        console.log('checked: '+ch);
+    });
+    $("#theform").submit(function(event){
+        event.preventDefault();
+        var phone = document.getElementById('phone').value;
+        var ref = document.getElementById('ref').value;
+        var groupid = document.getElementById('groupname').value;
+        var startDate='';
+        var endDate='';
+        if($('#calshow').is(":checked")) {
+
+            startDate = $('#reportrange').data('daterangepicker').startDate.format('YYYY-MM-DD');
+            endDate = $('#reportrange').data('daterangepicker').endDate.format('YYYY-MM-DD');
+            console.log('daterange '+startDate,endDate);
+        }
+
+        console.log(groupid+' '+phone+' '+ref);
+
+        //document.getElementById("current").innerHTML=groupid;
+        $.get({
+            type: 'get',
+            url: 'ajax/getItem.php',
+            dataType: 'json',
+            //data: $("#theform").serialize(),
+            data: {id: groupid, phone: phone, ref: ref, startDate: startDate, endDate: endDate},
+            error: function(response){console.log(response)},
+            success: function (response) {
+                //console.log(response);
+                var str='';
+                $.each(response, function(index, element) {
+
+                    str += "<tr><td>"+element.id+"</td>"+
+                    "<td>"+element.fulltimestamp+
+                    "<td>"+element.msisdn+"</td>"+
+                    "<td>"+element.account+"</td>"+
+                    "<td>"+element.service+"</td>"+
+                    "<td>"+element.reference+"</td>"+
+                    "<td>"+element.amount.toLocaleString()+"</td>"+
+                    "<td>"+status[element.tstatus]+"</td>"+
+                    "<td>"+lang[element.lang]+"</td>"+
+                    "<td>"+element.groupid+"</td>"+
+                    "</tr>";
+
+                    //console.log(str);
+                });
+                document.getElementById("transaction_table").innerHTML=str;
+            }
+        });
+    });
+
+
+
+});
+
+
+
